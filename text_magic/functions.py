4c7b3d8d449263135fcf6adc3cc74ca16bead54c
helenfern12 <scheuch.helen@gmail.com>
2025-11-20 11:25:01 +0100

    update
    working

--- text_magic/functions.py
+++ text_magic/functions.py
@@ -1,4 +1,5 @@
 from string import punctuation
+from nltk.corpus import stopwords
 
 PUNCTUATIONS = punctuation + "“”‘’—"
 
@@ -18,9 +19,17 @@ def clean_strip(text:str) -> str:
         text = text.replace("  ", " ")
     return text.strip()
 
-def pipeline(text:str, language) -> str:
+def clean_stopwords(text: str, language: str = "english"):
+    words = text.split(" ")
+    clean_text = []
+    for word in words: 
+        if words in stopwords.words(language):
+            clean_text.append(words)
+    return " ".join(clean_text)
+
+def pipeline(text:str, **kwargs) -> str:
     text = clean_to_lower(text)
     text = clean_punctuation(text)
     text = clean_strip(text)
-    text = clean_stopwords(text, language=language)
+    text = clean_stopwords(text, **kwargs)
     return text
